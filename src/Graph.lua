6012ecb2773ccb62bba49b736a77bed5778bcf6a
lordnaughty <lordnaughty@gmail.com>
2013-02-13 16:26:59 +0000

    Mostly finished subgraph isomorphsm algorithm.

--- src/Graph.lua
+++ src/Graph.lua
@@ -21,9 +21,19 @@ Graph = {}
 Graph.__index = Graph
 
 function Graph.new( vertices, edges )
+	vertices = vertices or {}
+	edges = edges or {}
+
+	local valences = {}
+
+	for vertex, peers in pairs(vertices) do
+		valences[vertex] = table.count(peers)
+	end
+
 	local result = {
-		vertices = vertices or {},
-		edges = edges or {},
+		vertices = vertices,
+		edges = edges,
+		valences = valences,
 	}
 
 	setmetatable(result, Graph)
@@ -36,6 +46,7 @@ end
 function Graph:_invariant()
 	local vertices = self.vertices
 	local edges = self.edges
+	local valences = self.valences
 
 	-- 1. Vertices are connected to other vertices by edges.
 	for vertex, peers in pairs(vertices) do
@@ -43,6 +54,7 @@ function Graph:_invariant()
 			assert(vertices[peer])
 			assert(edges[edge])
 		end
+		assert(valences[vertex] == table.count(peers))
 	end
 
 	-- 2. Edges are connected to two distinct vertices.
@@ -68,6 +80,7 @@ function Graph:addVertex( vertex )
 	assert(self.vertices[vertex] == nil)
 
 	self.vertices[vertex] = {}
+	self.valences[vertex] = 0
 
 	return vertex
 end
@@ -75,6 +88,7 @@ end
 function Graph:removeVertex( vertex )
 	local vertices = self.vertices
 	local edges = self.edges
+	local valences = self.valences
 	local peers = vertices[vertex]
 
 	if peers then
@@ -84,12 +98,15 @@ function Graph:removeVertex( vertex )
 		end
 
 		vertices[vertex] = nil
+		valences[vertex] = nil
 	end
 end
 
 function Graph:addEdge( edge, vertex1, vertex2 )
-	local peers1 = self.vertices[vertex1]
-	local peers2 = self.vertices[vertex2]
+	local vertices = self.vertices
+	local valences = self.valences
+	local peers1 = vertices[vertex1]
+	local peers2 = vertices[vertex2]
 
 	assert(self.edges[edge] == nil)
 	assert(vertex1 ~= vertex2) -- No loops.
@@ -99,6 +116,8 @@ function Graph:addEdge( edge, vertex1, vertex2 )
 	self.edges[edge] = { vertex1, vertex2 }
 	peers1[vertex2] = edge
 	peers2[vertex1] = edge
+	valences[vertex1] = valences[vertex1] + 1
+	valences[vertex2] = valences[vertex2] + 1
 end
 
 function Graph:removeEdge( edge )
@@ -107,10 +126,14 @@ function Graph:removeEdge( edge )
 
 	if endverts then
 		local vertices = self.vertices
+		local valences = self.valences
 		local vertex1, vertex2 = endverts[1], endverts[2]
 
 		vertices[vertex1][vertex2] = nil
 		vertices[vertex2][vertex1] = nil
+
+		valences[vertex1] = valences[vertex1] - 1
+		valences[vertex2] = valences[vertex2] - 1
 
 		edges[edge] = nil
 	end
@@ -124,6 +147,15 @@ function Graph:merge( other )
 	for edge, endverts in pairs(other.edges) do
 		self.edges[edge] = endverts
 	end
+
+	-- This could be made more efficient...
+	local valences = {}
+
+	for vertex, peers in pairs(self.vertices) do
+		valences[vertex] = table.count(peers)
+	end
+
+	self.valences = valences
 end
 
 -- This should be more efficient that distanceMap(), especially if queue
@@ -279,6 +311,178 @@ function Graph:betweenness()
 	-- end
 
 	return result, eccentricities, radius, diameter
+end
+
+local _defaultVertexEq = 
+	function ( host, hostVertex, pattern, patternVertex )
+		return true
+	end
+
+local _defaultEdgeEq = 
+	function (  host, hostVertex, pattern, patternVertex )
+		return true
+	end
+
+--
+-- Subgraph isomorphism based on J. R. Ullmann's 'An Algorithm for Subgraph
+-- Isomorphism'.
+--
+-- There is a planar subgraph isomorphism algorithm with far better asymptotic
+-- bounds (Eppstein 'Subgraph Isomorphism in Planar Graphs and Related
+-- Problems') but it's far more complicated.
+--
+-- Finds all matching pattern subgraphs in the self/host graph.
+--
+-- Returns false if there's no matches.
+--
+-- Returns true, matches: where matches is an array of vertex and edge maps.
+--
+function Graph:matches( pattern, vertexEq, edgeEq )
+	vertexEq = vertexEq or _defaultVertexEq
+	edgeEq = edgeEq or _defaultEdgeEq
+
+	-- The algorithm is a brute force search with a few additions to cut down
+	-- on the amount of work done.
+
+	-- The graph being enumerated is commonly called the host graph.
+	local host = self
+
+	-- Map from pattern vertices to host vertices that could potentially form
+	-- a subgraph. Used to cut down the amount of enumeration.
+	local potentials = {}
+
+	local hostValences = host.valences
+	local patternValences = pattern.valences
+
+	for patternVertex, _ in pairs(pattern.vertices) do
+		local patternValence = patternValences[patternVertex]
+		local array = {}
+
+		for hostVertex, hostValence in pairs(hostValences) do
+			-- If the host vertex has less peers than the pattern vertex a
+			-- match is impossible.
+			if hostValence >= patternValence and vertexEq(host, hostVertex, pattern, patternVertex) then
+				array[#array+1] = hostVertex
+			end
+		end
+
+		-- We if there is no host vertex that can be potentially matched to a
+		-- pattern vertex, there's no hope of any matches being found.
+		if #array == 0 then
+			return false
+		end
+
+		potentials[#potentials+1] = {
+			patternVertex = patternVertex,
+			hostVertices = array,
+		}
+	end
+
+	-- Now we perform a backtracking tree search in an attempt to create all
+	-- 1-to-1 maps (bijections) from pattern vertices to host vertices. We use
+	-- the potentials table to limit the maps we try and create to only those
+	-- that have a chance to succeed.
+	--
+	-- If we manage to get such a map we then check if the edges in the pattern
+	-- are present in the host. If they are we have found a subgraph! Add the
+	-- mapping to the result and carry on.
+
+	local result = {}
+	local maxDepth = #potentials
+	local depth = 1
+	
+	local stack = {}
+
+	for index = 1, #potentials do
+		local frame = {
+			choiceIndex = 1,
+			choices = potentials[index],
+			patternVertex = potentials[index].patternVertex,
+			hostVertex = nil,
+		}
+		stack[index] = frame
+	end
+
+	while true do
+		local frame = stack[depth]
+		local choices = frame.choices
+		local choice
+
+		for choiceIndex = frame.choiceIndex, #choices do
+			choice = choices[choiceIndex]
+
+			-- Does the choice conflict with a previous choice?
+			for index = depth-1, 1, -1 do
+				local ancestor = stack[index]
+				if choice == frame.hostVertex then
+					choice = nil
+					break
+				end
+			end
+
+			if choice then
+				frame.choiceIndex = choiceIndex + 1
+				break
+			end
+		end
+
+		if choice then
+			frame.hostVertex = choice
+			frame.choiceIndex = choiceIndex + 1
+
+			-- Have we got a mapping for each pattern vertex?
+			if depth == maxDepth then
+				-- pattern -> host vertex map.
+				local map = {}
+
+				for index = 1, #stack do
+					local frame = stack[index]
+					map[frame.patternVertex] = frame.hostVertex
+				end
+
+				local success = true
+
+				-- Do all pattern vertices that share an edge also share an
+				-- edge in the host graph?
+				for patternEdge, patternEdgeEnds in pairs(pattern.edges) do
+					local patternVertex1, patternVertex2 = patternEdgeEnds[1], patternEdgeEnds[2]
+
+					local hostVertex1, hostVertex2 = map[patternVertex1], map[patternVertex2]
+					assert(hostVertex1, hostVertex2)
+
+					local hostEdge = host.vertices[hostVertex1][hostVertex2]
+
+					if not hostEdge or not edgeEq(host, hostEdge, pattern, patternEdge) then
+						success = false
+						break
+					end
+				end
+
+				if success then
+					result[#result+1] = map
+				end
+			end
+		elseif depth == 1 then
+			if #result > 0 then
+				return true, result
+			else
+				return false
+			end
+		else
+			frame.choiceIndex = #choices + 1
+
+			-- We need to unwind.
+			for index = depth, 1, -1 do
+				local frame = stack[index]
+				if frame.choiceIndex > #frame.choices then
+					frame.choiceIndex = 1
+				else
+					depth = index
+					break
+				end
+			end
+		end
+	end
 end
 
 if arg then
